c1d39785f08e4d8fc6c47470198a48c9b715b962
zeronamoni <134900438+gogooma125732@users.noreply.github.com>
2026-07-05 14:16:56 +0900

    260705_2

--- sniffer_pcap.c
+++ sniffer_pcap.c
@@ -11,22 +11,25 @@
 #define ETHERTYPE_VLAN 0x8100
 #define HTTP_PREVIEW_LIMIT 2048
 
-struct ethheader {
+struct ethheader
+{
     u_char ether_dhost[ETHER_ADDR_LEN];
     u_char ether_shost[ETHER_ADDR_LEN];
     u_short ether_type;
 };
 
-struct vlanheader {
+struct vlanheader
+{
     u_short tci;
     u_short ether_type;
 };
 
-struct ipheader {
+struct ipheader
+{
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    unsigned char iph_ihl:4, iph_ver:4;
+    unsigned char iph_ihl : 4, iph_ver : 4;
 #else
-    unsigned char iph_ver:4, iph_ihl:4;
+    unsigned char iph_ver : 4, iph_ihl : 4;
 #endif
     unsigned char iph_tos;
     unsigned short int iph_len;
@@ -39,7 +42,8 @@ struct ipheader {
     struct in_addr iph_destip;
 };
 
-struct tcpheader {
+struct tcpheader
+{
     u_short tcp_sport;
     u_short tcp_dport;
     u_int tcp_seq;
@@ -71,21 +75,31 @@ static void print_mac(const u_char *mac)
 static void print_tcp_flags(u_char flags)
 {
     int printed = 0;
-    struct {
+    struct
+    {
         u_char bit;
         const char *name;
     } flag_names[] = {
-        {TH_SYN, "SYN"}, {TH_ACK, "ACK"}, {TH_FIN, "FIN"}, {TH_RST, "RST"},
-        {TH_PUSH, "PSH"}, {TH_URG, "URG"}, {TH_ECE, "ECE"}, {TH_CWR, "CWR"},
+        {TH_SYN, "SYN"},
+        {TH_ACK, "ACK"},
+        {TH_FIN, "FIN"},
+        {TH_RST, "RST"},
+        {TH_PUSH, "PSH"},
+        {TH_URG, "URG"},
+        {TH_ECE, "ECE"},
+        {TH_CWR, "CWR"},
     };
 
-    for (size_t i = 0; i < sizeof(flag_names) / sizeof(flag_names[0]); i++) {
-        if (flags & flag_names[i].bit) {
+    for (size_t i = 0; i < sizeof(flag_names) / sizeof(flag_names[0]); i++)
+    {
+        if (flags & flag_names[i].bit)
+        {
             printf("%s%s", printed ? "," : "", flag_names[i].name);
             printed = 1;
         }
     }
-    if (!printed) {
+    if (!printed)
+    {
         printf("NONE");
     }
 }
@@ -94,13 +108,16 @@ static int is_http_like(const u_char *payload, int len)
 {
     const char *methods[] = {"GET ", "POST ", "HEAD ", "PUT ", "DELETE ", "OPTIONS ", "PATCH ", "HTTP/"};
 
-    if (len < 4) {
+    if (len < 4)
+    {
         return 0;
     }
 
-    for (size_t i = 0; i < sizeof(methods) / sizeof(methods[0]); i++) {
+    for (size_t i = 0; i < sizeof(methods) / sizeof(methods[0]); i++)
+    {
         size_t method_len = strlen(methods[i]);
-        if ((size_t)len >= method_len && memcmp(payload, methods[i], method_len) == 0) {
+        if ((size_t)len >= method_len && memcmp(payload, methods[i], method_len) == 0)
+        {
             return 1;
         }
     }
@@ -111,17 +128,21 @@ static int is_http_like(const u_char *payload, int len)
 static int contains_case_insensitive(const char *buf, const char *needle)
 {
     size_t needle_len = strlen(needle);
-    if (needle_len == 0) {
+    if (needle_len == 0)
+    {
         return 1;
     }
 
-    for (const char *p = buf; *p; p++) {
+    for (const char *p = buf; *p; p++)
+    {
         size_t i = 0;
         while (i < needle_len && p[i] &&
-               tolower((unsigned char)p[i]) == tolower((unsigned char)needle[i])) {
+               tolower((unsigned char)p[i]) == tolower((unsigned char)needle[i]))
+        {
             i++;
         }
-        if (i == needle_len) {
+        if (i == needle_len)
+        {
             return 1;
         }
     }
@@ -134,7 +155,8 @@ static void print_http_message(const u_char *payload, int payload_len)
     int preview_len = payload_len > HTTP_PREVIEW_LIMIT ? HTTP_PREVIEW_LIMIT : payload_len;
     char preview[HTTP_PREVIEW_LIMIT + 1];
 
-    for (int i = 0; i < preview_len; i++) {
+    for (int i = 0; i < preview_len; i++)
+    {
         preview[i] = (isprint(payload[i]) || payload[i] == '\r' ||
                       payload[i] == '\n' || payload[i] == '\t')
                          ? (char)payload[i]
@@ -147,15 +169,18 @@ static void print_http_message(const u_char *payload, int payload_len)
     printf("%s\n", preview);
     printf("----------------------------------------\n");
 
-    if (contains_case_insensitive(preview, "Authorization:")) {
+    if (contains_case_insensitive(preview, "Authorization:"))
+    {
         printf("[Forensic Note] Authorization header detected. Plain HTTP can expose credentials.\n");
     }
-    if (contains_case_insensitive(preview, "Cookie:")) {
+    if (contains_case_insensitive(preview, "Cookie:"))
+    {
         printf("[Forensic Note] Cookie header detected. Session data may be exposed on plaintext HTTP.\n");
     }
     if (contains_case_insensitive(preview, "password=") ||
         contains_case_insensitive(preview, "passwd=") ||
-        contains_case_insensitive(preview, "token=")) {
+        contains_case_insensitive(preview, "token="))
+    {
         printf("[Forensic Note] Credential-like parameter detected in HTTP payload.\n");
     }
 }
@@ -165,7 +190,8 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
     (void)args;
     packet_no++;
 
-    if (header->caplen < sizeof(struct ethheader)) {
+    if (header->caplen < sizeof(struct ethheader))
+    {
         printf("[Packet %d] Truncated Ethernet frame\n", packet_no);
         return;
     }
@@ -174,8 +200,10 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
     uint16_t ether_type = ntohs(eth->ether_type);
     int ethernet_len = sizeof(struct ethheader);
 
-    if (ether_type == ETHERTYPE_VLAN) {
-        if (header->caplen < sizeof(struct ethheader) + sizeof(struct vlanheader)) {
+    if (ether_type == ETHERTYPE_VLAN)
+    {
+        if (header->caplen < sizeof(struct ethheader) + sizeof(struct vlanheader))
+        {
             printf("[Packet %d] Truncated VLAN frame\n", packet_no);
             return;
         }
@@ -184,11 +212,13 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
         ethernet_len += sizeof(struct vlanheader);
     }
 
-    if (ether_type != ETHERTYPE_IP) {
+    if (ether_type != ETHERTYPE_IP)
+    {
         return;
     }
 
-    if (header->caplen < (bpf_u_int32)(ethernet_len + (int)sizeof(struct ipheader))) {
+    if (header->caplen < (bpf_u_int32)(ethernet_len + (int)sizeof(struct ipheader)))
+    {
         printf("[Packet %d] Truncated IP header\n", packet_no);
         return;
     }
@@ -197,22 +227,26 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
     int ip_header_len = ip->iph_ihl * 4;
     int ip_total_len = ntohs(ip->iph_len);
 
-    if (ip->iph_ver != 4 || ip_header_len < 20) {
+    if (ip->iph_ver != 4 || ip_header_len < 20)
+    {
         printf("[Packet %d] Invalid IPv4 header\n", packet_no);
         return;
     }
 
-    if (ip->iph_protocol != IPPROTO_TCP) {
+    if (ip->iph_protocol != IPPROTO_TCP)
+    {
         return;
     }
 
     if (ip_total_len < ip_header_len ||
-        header->caplen < (bpf_u_int32)(ethernet_len + ip_total_len)) {
+        header->caplen < (bpf_u_int32)(ethernet_len + ip_total_len))
+    {
         printf("[Packet %d] Truncated IPv4 packet\n", packet_no);
         return;
     }
 
-    if (header->caplen < (bpf_u_int32)(ethernet_len + ip_header_len + (int)sizeof(struct tcpheader))) {
+    if (header->caplen < (bpf_u_int32)(ethernet_len + ip_header_len + (int)sizeof(struct tcpheader)))
+    {
         printf("[Packet %d] Truncated TCP header\n", packet_no);
         return;
     }
@@ -220,7 +254,8 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
     const struct tcpheader *tcp = (const struct tcpheader *)(packet + ethernet_len + ip_header_len);
     int tcp_header_len = TH_OFF(tcp) * 4;
 
-    if (tcp_header_len < 20 || ip_total_len < ip_header_len + tcp_header_len) {
+    if (tcp_header_len < 20 || ip_total_len < ip_header_len + tcp_header_len)
+    {
         printf("[Packet %d] Invalid TCP header length\n", packet_no);
         return;
     }
@@ -233,42 +268,45 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
            header->caplen, header->len);
 
     printf("Ethernet Header\n");
-    printf("  Src MAC : ");
+    printf("  MAC: ");
     print_mac(eth->ether_shost);
-    printf("\n");
-    printf("  Dst MAC : ");
+    printf(" / ");
     print_mac(eth->ether_dhost);
     printf("\n");
 
     printf("IP Header\n");
-    printf("  Src IP  : %s\n", inet_ntoa(ip->iph_sourceip));
-    printf("  Dst IP  : %s\n", inet_ntoa(ip->iph_destip));
-    printf("  IP Header Length: %d bytes\n", ip_header_len);
-    printf("  IP Total Length : %d bytes\n", ip_total_len);
-    printf("  TTL: %u\n", ip->iph_ttl);
+    printf("  IP: %s / %s\n", inet_ntoa(ip->iph_sourceip), inet_ntoa(ip->iph_destip));
+    printf("  IP Header Length: %d bytes | IP Total Length : %d bytes | TTL: %u\n", ip_header_len, ip_total_len, ip->iph_ttl);
 
     printf("TCP Header\n");
-    printf("  Src Port: %u\n", ntohs(tcp->tcp_sport));
-    printf("  Dst Port: %u\n", ntohs(tcp->tcp_dport));
+    printf("  Port: %u / %u\n", ntohs(tcp->tcp_sport), ntohs(tcp->tcp_dport));
     printf("  TCP Header Length: %d bytes\n", tcp_header_len);
     printf("  Flags: ");
     print_tcp_flags(tcp->tcp_flags);
     printf("\n");
 
-    if ((tcp->tcp_flags & TH_SYN) && !(tcp->tcp_flags & TH_ACK)) {
-        printf("[Forensic Note] SYN without ACK. This may be a connection attempt or scan indicator.\n");
+    if ((tcp->tcp_flags & TH_SYN) && !(tcp->tcp_flags & TH_ACK))
+    {
+        printf("[Note] SYN without ACK. This may be a connection attempt or scan indicator.\n");
     }
-    if (tcp->tcp_flags & TH_RST) {
-        printf("[Forensic Note] RST observed. This can appear in rejected connections or scan responses.\n");
+    if (tcp->tcp_flags & TH_RST)
+    {
+        printf("[Note] RST observed. This can appear in rejected connections or scan responses.\n");
     }
 
-    if (payload_len > 0) {
-        if (is_http_like(payload, payload_len)) {
+    if (payload_len > 0)
+    {
+        if (is_http_like(payload, payload_len))
+        {
             print_http_message(payload, payload_len);
-        } else {
+        }
+        else
+        {
             printf("Application Data: %d bytes, not recognized as plaintext HTTP\n", payload_len);
         }
-    } else {
+    }
+    else
+    {
         printf("Application Data: none\n");
     }
 }
@@ -291,36 +329,46 @@ int main(int argc, char *argv[])
     const char *filter_exp = "tcp";
     bpf_u_int32 net = PCAP_NETMASK_UNKNOWN;
 
-    if (argc < 3) {
+    if (argc < 3)
+    {
         usage(argv[0]);
         return EXIT_FAILURE;
     }
 
-    if (argc >= 4) {
+    if (argc >= 4)
+    {
         filter_exp = argv[3];
     }
 
-    if (strcmp(argv[1], "-r") == 0) {
+    if (strcmp(argv[1], "-r") == 0)
+    {
         handle = pcap_open_offline(argv[2], errbuf);
-    } else if (strcmp(argv[1], "-i") == 0) {
+    }
+    else if (strcmp(argv[1], "-i") == 0)
+    {
         handle = pcap_open_live(argv[2], BUFSIZ, 1, 1000, errbuf);
-    } else {
+    }
+    else
+    {
         usage(argv[0]);
         return EXIT_FAILURE;
     }
 
-    if (handle == NULL) {
+    if (handle == NULL)
+    {
         fprintf(stderr, "Could not open source: %s\n", errbuf);
         return EXIT_FAILURE;
     }
 
-    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1)
+    {
         pcap_perror(handle, "pcap_compile error");
         pcap_close(handle);
         return EXIT_FAILURE;
     }
 
-    if (pcap_setfilter(handle, &fp) != 0) {
+    if (pcap_setfilter(handle, &fp) != 0)
+    {
         pcap_perror(handle, "pcap_setfilter error");
         pcap_freecode(&fp);
         pcap_close(handle);
